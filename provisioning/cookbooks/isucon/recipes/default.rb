c97529f8d1a04c1bbb2627410f53e5892dbd1900
hanaclover <tosi_kai_tosi@yahoo.co.jp>
2019-06-08 00:54:06 +0900

    typo

--- provisioning/cookbooks/isucon/recipes/default.rb
+++ provisioning/cookbooks/isucon/recipes/default.rb
@@ -366,7 +366,7 @@ go build
 
 ## ruby
 sudo yum install -y gcc git wget bzip2
-wget -O ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.
+wget -O ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz
 tar -xzvf ruby-install-0.7.0.tar.gz
 cd ruby-install-0.7.0/
 sudo make install
